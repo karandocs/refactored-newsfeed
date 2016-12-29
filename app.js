847298abd983a2ebec29408ca36ed6ec047726af
karankrizz <kirubakarann7@gmail.com>
2016-12-29 05:57:54 +0530

    comments added for post

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+    url: '/posts/{id}',
+    templateUrl: '/posts.html',
+    controller: 'PostsCtrl'
+  });
+  $urlRouterProvider.otherwise('home');
+  }])
 .factory('posts', [function(){
   var o = {
     posts: []
   };
   return o;
 }])
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+};
+}])
+
 .controller('MainCtrl', [
 '$scope',
 'posts',
@@ -25,10 +59,14 @@ $scope.addPost = function(){
   $scope.posts.push({
     title: $scope.title,
     link: $scope.link,
-    upvotes: 0
+    upvotes: 0,
+    comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
   });
   $scope.title = '';
   $scope.link = '';
 };
 
-}]);
+}])
